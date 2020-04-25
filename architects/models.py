ae5ff872a46037c7c8c423996b4cdabf173b6a12
Norma Driske <norma@fastmail.com>
2020-04-26 00:07:33 +0200

    Add architect image

--- architects/models.py
+++ architects/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class ArchitectsIndexPage(Page):
@@ -16,11 +17,18 @@ class ArchitectPage(Page):
     birthday = models.DateField("Birthday", blank=True, null=True)
     day_of_death = models.DateField("Day of Death", blank=True, null=True)
     description = RichTextField(blank=True)
-
+    image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
     content_panels = Page.content_panels + [
         FieldPanel("first_name"),
         FieldPanel("last_name"),
         FieldPanel("birthday"),
         FieldPanel("day_of_death"),
         FieldPanel("description", classname="full"),
+        ImageChooserPanel("image"),
     ]
