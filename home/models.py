d4be15f2ab2501389f3f872283ef732b9e2e3054
Norma Driske <norma@fastmail.com>
2020-04-25 22:45:40 +0200

    Add hero text field

--- home/models.py
+++ home/models.py
@@ -5,8 +5,9 @@ from wagtail.core.models import Page
 
 
 class HomePage(Page):
+    hero_text = models.CharField(max_length=400, blank=True)
     body = RichTextField(blank=True)
-
     content_panels = Page.content_panels + [
+        FieldPanel("hero_text", classname="full"),
         FieldPanel("body", classname="full"),
     ]
