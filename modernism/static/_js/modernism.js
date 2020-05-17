37d28b10f321325f808c171f06033ae51c633823
Norma Driske <norma@fastmail.com>
2020-05-17 19:56:09 +0200

    Toggle burger menu

--- modernism/static/_js/modernism.js
+++ modernism/static/_js/modernism.js
@@ -3,10 +3,10 @@ const NAV_BURGER = document.querySelector('.navbar-burger');
 const toggleBurgerMenu = () => {
     document.querySelector('.navbar-menu').classList.toggle('is-active');
     NAV_BURGER.classList.toggle('is-active');
-};
+}
 
 const addClickEventListenerToBurgerMenu = () => {
     NAV_BURGER.addEventListener('click', toggleBurgerMenu);
-};
+}
 
 addClickEventListenerToBurgerMenu();
