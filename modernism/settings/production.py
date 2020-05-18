2f4cab2527bf4968510dfa08a70c8d1b625cf421
Norma Driske <norma@fastmail.com>
2020-05-18 11:54:23 +0200

    Add whitenoise

--- modernism/settings/production.py
+++ modernism/settings/production.py
@@ -17,6 +17,15 @@ SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 ALLOWED_HOSTS = ["*"]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+COMPRESS_OFFLINE = True
+COMPRESS_CSS_FILTERS = [
+    "compressor.filters.css_default.CssAbsoluteFilter",
+    "compressor.filters.cssmin.CSSMinFilter",
+]
+COMPRESS_CSS_HASHING_METHOD = "content"
+
 try:
     from .local import *
 except ImportError:
