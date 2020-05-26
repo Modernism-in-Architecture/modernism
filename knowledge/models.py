16ee57bdb57964dd21dba9c9989d8dcd8cfd4608
Norma Driske <norma@fastmail.com>
2020-05-26 16:03:59 +0200

    Create facts page and tags

--- knowledge/models.py
+++ knowledge/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from modelcluster.fields import ParentalKey
+from taggit.models import TaggedItemBase
 from wagtail.admin.edit_handlers import FieldPanel
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
@@ -10,6 +13,19 @@ class KnowledgeIndexPage(Page):
     subpage_types = ["KnowledgePage"]
     content_panels = Page.content_panels + [FieldPanel("intro", classname="full")]
 
+    def get_context(self, request):
+        context = super().get_context(request)
+
+        context["facts"] = KnowledgePage.objects.live().order_by("title")
+
+        return context
+
+
+class KnowledgePageTag(TaggedItemBase):
+    content_object = ParentalKey(
+        "knowledge.KnowledgePage", on_delete=models.CASCADE, related_name="tagged_items"
+    )
+
 
 class KnowledgePage(Page):
     description = RichTextField(blank=True)
@@ -20,7 +36,18 @@ class KnowledgePage(Page):
         on_delete=models.SET_NULL,
         related_name="+",
     )
+    tags = ClusterTaggableManager(through=KnowledgePageTag, blank=True)
     content_panels = Page.content_panels + [
         FieldPanel("description", classname="full"),
         ImageChooserPanel("image"),
+        FieldPanel("tags"),
     ]
+
+    @property
+    def get_tags(self):
+        tags = self.tags.all()
+        for tag in tags:
+            tag.url = "/" + "/".join(
+                s.strip("/") for s in [self.get_parent().url, "tags", tag.slug]
+            )
+        return tags
