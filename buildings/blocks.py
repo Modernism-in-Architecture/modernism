44997d0330b643cf890fdb02dc1c58161a338417
Norma Driske <hello@normade.dev>
2021-02-07 18:44:16 +0100

    Add image urls to page api response

--- buildings/blocks.py
+++ buildings/blocks.py
@@ -1,5 +1,15 @@
 from wagtail.core.blocks import CharBlock, StructBlock, TextBlock
-from wagtail.images.blocks import ImageChooserBlock
+from wagtail.images.blocks import ImageChooserBlock as DefaultImageChooserBlock
+
+
+class ImageChooserBlock(DefaultImageChooserBlock):
+    def get_api_representation(self, value, context=None):
+        if value:
+            return {
+                "id": value.id,
+                "title": value.title,
+                "large": value.get_rendition("max-1300x700").attrs_dict,
+            }
 
 
 class GalleryImageBlock(StructBlock):
