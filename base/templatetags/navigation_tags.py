3034c0d18127370a742bbf0f577a042ff8a49650
Norma Driske <hello@normade.dev>
2021-03-25 23:07:12 +0100

    Remove person from menu

--- base/templatetags/navigation_tags.py
+++ base/templatetags/navigation_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from home.models import GeneralPage
 from wagtail.core.models import Page, Site
 
 register = template.Library()
@@ -15,14 +16,37 @@ def get_site_root(context):
 
 @register.inclusion_tag("tags/main_menu.html", takes_context=True)
 def main_menu(context, parent, calling_page=None):
-    menuitems = parent.get_children().live().in_menu()
+    menuitems = Page.objects.not_type(GeneralPage).live().in_menu()
+
     for menuitem in menuitems:
-        menuitem.show_dropdown = has_menu_children(menuitem)
+        menuitem.is_parent = has_menu_children(menuitem)
+
         menuitem.active = (
             calling_page.url_path.startswith(menuitem.url_path)
             if calling_page
             else False
         )
+
+    return {
+        "calling_page": calling_page,
+        "menuitems": menuitems,
+        "request": context["request"],
+    }
+
+
+@register.inclusion_tag("tags/second_menu.html", takes_context=True)
+def second_menu(context, parent, calling_page=None):
+    menuitems = GeneralPage.objects.live().in_menu()
+
+    for menuitem in menuitems:
+        menuitem.is_parent = has_menu_children(menuitem)
+
+        menuitem.active = (
+            calling_page.url_path.startswith(menuitem.url_path)
+            if calling_page
+            else False
+        )
+
     return {
         "calling_page": calling_page,
         "menuitems": menuitems,
@@ -36,24 +60,6 @@ def has_menu_children(page):
 
 def has_children(page):
     return page.get_children().live().exists()
-
-
-@register.inclusion_tag("tags/main_menu_children.html", takes_context=True)
-def main_menu_children(context, parent, calling_page=None):
-    menuitems_children = parent.get_children()
-    menuitems_children = menuitems_children.live().in_menu()
-    for menuitem in menuitems_children:
-        menuitem.has_dropdown = has_menu_children(menuitem)
-        menuitem.active = (
-            calling_page.url_path.startswith(menuitem.url_path)
-            if calling_page
-            else False
-        )
-    return {
-        "parent": parent,
-        "menuitems_children": menuitems_children,
-        "request": context["request"],
-    }
 
 
 @register.inclusion_tag("tags/breadcrumbs.html", takes_context=True)
