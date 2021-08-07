0a0354b84479250e5f76fabeca950563ad84b263
Norma Driske <hello@normade.dev>
2021-08-07 10:17:55 +0200

    Remove unused code

--- modernism/urls.py
+++ modernism/urls.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
-from django.views.i18n import JavaScriptCatalog
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
@@ -12,7 +11,6 @@ from .api import api_router
 urlpatterns = [
     url(r"^django-admin/", admin.site.urls),
     url(r"^admin/", include(wagtailadmin_urls)),
-    # url(r"^jsi18n/", JavaScriptCatalog.as_view(), name="javascript-catalog"),
     url(r"^documents/", include(wagtaildocs_urls)),
     url(r"^api/v2/", api_router.urls),
 ]
