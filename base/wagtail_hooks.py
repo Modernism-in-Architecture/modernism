2be5d6ddb82f8ea5e669c8aa426bdaed4434a56c
Norma Driske <hello@normade.dev>
2021-11-14 02:00:32 +0100

    Remove old apps

--- base/wagtail_hooks.py
+++
@@ -1,6 +0,0 @@
-from wagtail.core import hooks
-
-
-@hooks.register("construct_main_menu")
-def hide_explorer_menu_item(request, menu_items):
-    menu_items[:] = [item for item in menu_items if item.name != "documents"]
