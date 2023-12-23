cd183aa12b54bf2cd0214ebfaf2b70f20168048d
Dmytro Klymenko <dklymenko.work@gmail.com>
2023-12-23 16:11:46 +0100

    call button in the script

--- modernism/static/lib/modernism.js
+++ modernism/static/lib/modernism.js
@@ -64,3 +64,11 @@ window.onload = () => {
         currentMenuItem.parentElement.parentElement.classList.toggle('show');
     }
 };
+
+addBackToTop({
+    showWhenScrollTopIs: screen.height * 2,
+    diameter: 50,
+    backgroundColor: '#0598f7',
+    innerHTML: '<svg viewBox="0 0 24 24"><path d="M4 12l1.41 1.41L11 7.83V20h2V7.83l5.58 5.59L20 12l-8-8-8 8z"/></svg>',
+    textColor: '#ffffff'
+});
