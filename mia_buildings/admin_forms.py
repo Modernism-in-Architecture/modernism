43a6fa2845e428e3601602bf57577468d82446d1
Norma Driske <hello@normade.dev>
2024-01-08 01:25:23 +0100

    Override clean_history and clean_descripiton

--- mia_buildings/admin_forms.py
+++ mia_buildings/admin_forms.py
@@ -1,33 +1,136 @@
+from taggit.models import Tag
+from tinymce.widgets import TinyMCE
+
+from django.contrib.admin.widgets import FilteredSelectMultiple
 from django.core.exceptions import ValidationError
-from django.forms import BaseInlineFormSet, ModelForm
+from django.forms import (
+    CharField,
+    ChoiceField,
+    ClearableFileInput,
+    Form,
+    ImageField,
+    ModelChoiceField,
+    ModelForm,
+    ModelMultipleChoiceField,
+    Select,
+    Textarea,
+)
+from django.forms.widgets import MultipleHiddenInput
 
-# from mia_buildings.admin_utils import AdminUtils
+from mia_buildings.admin_utils import validate_content_markup
 from mia_buildings.models import Building
+from mia_facts.models import Photographer
 
-#
-# class BuildingForm(ModelForm):  # noqa: DJ06
-#     class Meta:
-#         model = Building
-#         exclude = []
-#
-#     def clean_description(self):
-#         description = self.cleaned_data.get("description")
-#
-#         success, msg = AdminUtils.validate_html(description)
-#         if not success:
-#             raise ValidationError(f"{msg}")
-#         return self.cleaned_data.get("description")
-#
-#
-# class BuildingFormSet(BaseInlineFormSet):
-#     def clean(self):
-#         super(BuildingFormSet, self).clean()
-#
-#         for form in self.forms:
-#             if not form.is_valid():
-#                 return
-#             if form.cleaned_data and not form.cleaned_data.get("DELETE", False):
-#                 description = form.cleaned_data["description"]
-#                 success, msg = AdminUtils.validate_html(description)
-#                 if not success:
-#                     raise ValidationError(f"{msg}")
+
+class MultipleImageFileInput(ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class MultipleImageFileField(ImageField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleImageFileInput())
+        super().__init__(*args, **kwargs)
+
+    def clean(self, data, initial=None):
+        single_file_clean = super().clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+        else:
+            result = single_file_clean(data, initial)
+        return result
+
+
+class BulkUploadImagesForm(Form):
+    multiple_images = MultipleImageFileField(label="Select images")
+    photographer = ChoiceField(required=False, widget=Select, choices=[])
+    tags = ModelMultipleChoiceField(
+        required=False,
+        queryset=Tag.objects.order_by("name"),
+        widget=FilteredSelectMultiple("Tags", True),
+    )
+    title = CharField(label="General name for the images")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_photographer_choices()
+
+    def set_photographer_choices(self):
+        photographers = Photographer.objects.values_list("id", "last_name").order_by(
+            "last_name"
+        )
+        choices = [(None, "------")] + list(photographers)
+        self.fields["photographer"].choices = choices
+
+
+class BuildingAdminForm(ModelForm):
+    multiple_images = MultipleImageFileField(required=False)
+    photographer = ChoiceField(
+        required=False,
+        widget=Select,
+    )
+
+    class Meta:
+        model = Building
+        widgets = {
+            "subtitle": Textarea(attrs={"rows": "2"}),
+            "seo_title": Textarea(attrs={"rows": "2"}),
+            "history": TinyMCE(
+                mce_attrs={"convert_urls": False, "browser_spellcheck": True}
+            ),
+            "description": TinyMCE(
+                mce_attrs={"convert_urls": False, "browser_spellcheck": True}
+            ),
+            "directions": Textarea(attrs={"rows": "3"}),
+            "address": Textarea(attrs={"rows": "3"}),
+        }
+        fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_photographer_choices()
+
+    def set_photographer_choices(self):
+        photographers = Photographer.objects.values_list("id", "last_name").order_by(
+            "last_name"
+        )
+        choices = [("", "------")] + list(photographers)
+        self.fields["photographer"].choices = choices
+
+    def clean_description(self):
+        description = self.cleaned_data.get("description")
+        success, cleaned_content_data = validate_content_markup(description)
+
+        if not success:
+            # Solved in this unconventional way to provide the cleaned data to the user in an actually immutable form
+            copied_form_data = self.data.copy()
+            copied_form_data["description"] = cleaned_content_data
+            self.data = copied_form_data
+            raise ValidationError(
+                f"Your text markup needed to be cleaned. Please review, correct if necessary and save again."
+            )
+
+        return description
+
+    def clean_history(self):
+        history_content = self.cleaned_data.get("history")
+        success, cleaned_content_data = validate_content_markup(history_content)
+
+        if not success:
+            # Solved in this unconventional way to provide the cleaned data to the user in an actually immutable form
+            copied_form_data = self.data.copy()
+            copied_form_data["history"] = cleaned_content_data
+            self.data = copied_form_data
+            raise ValidationError(
+                f"Your text markup needed to be cleaned. Please review, correct if necessary and save again."
+            )
+
+        return history_content
+
+
+class BuildingForImageSelectionAdminForm(Form):
+    _selected_action = CharField(widget=MultipleHiddenInput)
+    _images = CharField(widget=MultipleHiddenInput)
+    building = ModelChoiceField(
+        queryset=Building.objects.all().order_by("name"),
+        widget=Select(),
+    )
