71cf08761ac96f275e07dc3b7773cc6cd7df2b35
Norma Driske <hello@normade.dev>
2024-01-08 01:24:12 +0100

    Add validation function

---
+++ mia_buildings/admin_utils.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+
+
+def validate_content_markup(html: str) -> (bool, str):
+    soup = BeautifulSoup(html, "html.parser")
+    before = soup.prettify()
+    cleanup(soup)
+    after = soup.prettify()
+    return before == after, after
+
+
+def cleanup(document: BeautifulSoup) -> None:
+    ATTRIBUTE_WHITELIST = ["href", "src"]
+    ELEMENT_WHITELIST = ["p", "a", "em", "strong"]
+
+    def removeAttributes(element):
+        for attr in set(element.attrs):
+            if attr not in ATTRIBUTE_WHITELIST:
+                del element[attr]
+
+    def removeEmptyElement(element):
+        if len(element.get_text(strip=True)) == 0:
+            element.extract()
+
+    def unwrapElement(element):
+        if element.name not in ELEMENT_WHITELIST:
+            element.unwrap()
+
+    def cleanupElement(element):
+        removeAttributes(element)
+        unwrapElement(element)
+        removeEmptyElement(element)
+
+    result = document.find_all()
+    for entry in result:
+        cleanupElement(entry)
