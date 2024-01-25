4ec687aefda39338a2a2ed5e54255a99455859e5
Norma Driske <hello@normade.dev>
2024-01-26 00:28:34 +0100

    Refactor mixing to dry

--- modernism/mixins.py
+++ modernism/mixins.py
@@ -4,31 +4,23 @@ from modernism.tools import validate_and_clean_content_markup
 
 
 class ContentMarkupMixin:
-    def clean_description(self):
-        description = self.cleaned_data.get("description")
-        was_clean, cleaned_content_data = validate_and_clean_content_markup(description)
-
-        if not was_clean:
-            copied_form_data = self.data.copy()
-            copied_form_data["description"] = cleaned_content_data
-            self.data = copied_form_data
-            raise ValidationError(
-                f"Your text markup needed to be cleaned. Please review, correct if necessary and save again."
-            )
-
-        return description
-
-    def clean_history(self):
-        history_content = self.cleaned_data.get("history")
-        clean, cleaned_content_data = validate_and_clean_content_markup(history_content)
+    def _clean_content_markup_field(self, field_name):
+        content_data = self.cleaned_data.get(field_name)
+        clean, cleaned_content_data = validate_and_clean_content_markup(content_data)
 
         if not clean:
             # Solved in this unconventional way to provide the cleaned data to the user for review
             copied_form_data = self.data.copy()
-            copied_form_data["history"] = cleaned_content_data
+            copied_form_data[field_name] = cleaned_content_data
             self.data = copied_form_data
             raise ValidationError(
-                f"Your text markup needed to be cleaned. Please review, correct if necessary and save again."
+                f"Your text markup in {field_name} needed to be cleaned. Please review, correct if necessary, and save again."
             )
 
-        return history_content
+        return content_data
+
+    def clean_description(self):
+        return self._clean_content_markup_field("description")
+
+    def clean_history(self):
+        return self._clean_content_markup_field("history")
