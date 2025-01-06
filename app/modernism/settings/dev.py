5b2df8cc26923f9f4e17e15039b70e20f8b5bfed
Norma Driske <hello@normade.dev>
2025-01-06 11:46:43 +0100

    Enable toolbar in DEBUG

--- app/modernism/settings/dev.py
+++ app/modernism/settings/dev.py
@@ -5,11 +5,3 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
-
-INSTALLED_APPS += [
-    "debug_toolbar",
-]
-
-MIDDLEWARE = [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-] + MIDDLEWARE
